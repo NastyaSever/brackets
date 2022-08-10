9185fc6470fa44a78ef2e9212e111478b96e7c2d
Nastya Severina <Severina.Nastya1992@yandex.by>
2022-08-10 22:39:51 +0300

    update: half part complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const openBrackets = ['(', '{', '['];
+  const bracketsPair = {
+    [')']:'(',
+    ['}']:'{',
+    [']']:'[',
+  };
+
+  function isBracketsHasPair(str) {
+    let stack = [];
+    for(let i = 0; i < str.length; i++) {
+      let currentBracket = str[i];
+      if(openBrackets.includes(currentBracket)) {
+        stack.push(currentBracket);
+      } else {
+        if(stack.length === 0) {
+          return false;
+        }
+
+        let topElement = stack[stack.length - 1];
+        if(bracketsPair[currentBracket] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0;
+  } return isBracketsHasPair(str)
+
+};
